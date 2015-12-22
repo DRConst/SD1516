b9521f007de2c9ff0c9639704c64ca0f9a31f82d
Diogo Constancio <drconstancio@hotmail.com>
2015-12-22 15:43:52 +0000

    Added Serializer and protocol docs

--- src/Commons/Serializer.java
+++ src/Commons/Serializer.java
@@ -1,5 +1,9 @@
 package Commons;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import java.io.*;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,29 +12,37 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Serializer {
     HashMap<String, ReentrantLock> locks;
-    ReentrantLock hashLock = new ReentrantLock();
+    ReentrantLock hashLock;
 
+    public Serializer()
+    {
+        locks = new HashMap<>();
+        hashLock  = new ReentrantLock();
+    }
     public Object readObject(String name) throws IOException, ClassNotFoundException {
         Object toRet = null;
-        if(locks.containsKey(name)) // Something is writing to the file
+        if(!locks.containsKey(name))
         {
-            locks.get(name).lock();
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(name + ".saved"))) {
-                toRet = ois.readObject();
-            }
-            locks.get(name).unlock();
+            //First time writing to file, init lock
+            hashLock.lock();//Make sure we don't get race conditions creating locks
+            locks.put(name, new ReentrantLock());
+            hashLock.unlock();
         }
-        else
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(name + ".saved"))) {
-                toRet = ois.readObject();
-            }
+        locks.get(name).lock();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(name + ".saved"))) {
+            toRet = ois.readObject();
+        }catch (IOException e)
+        {
+            return null;
+        }
+        locks.get(name).unlock();
 
         return toRet;
     }
 
     public void writeObject(Object o) throws IOException { //Multithreading might pose problems with concurrent writes so lock the file
 
-       
+
         String name = o.getClass().getName();
 
         if(!locks.containsKey(name))
@@ -46,6 +58,42 @@ public class Serializer {
         }
         locks.get(name).unlock();
     }
+
+    public static String serializeToString(Object o)
+    {
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bao);
+            oos.writeObject(o);
+            return new BASE64Encoder().encode(bao.toByteArray()).toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public static Object unserializeFromString(String s)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Object o = null;
+        try {
+            baos.write(s.getBytes("UTF-8"));
+            ObjectInputStream oos = new ObjectInputStream(new ByteArrayInputStream(new BASE64Decoder().decodeBuffer(s)));
+            o = oos.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return o;
+    }
+
+
 
 
 
